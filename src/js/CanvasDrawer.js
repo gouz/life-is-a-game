32ff8a2435eac0e452e99e6ed21a3bd90c84096d
Sylvain Gougouzian <sylvain@lidrea.fr>
2022-03-26 21:43:26 +0100

    feat: :recycle: comment the calculus

--- src/js/CanvasDrawer.js
+++ src/js/CanvasDrawer.js
@@ -9,28 +9,29 @@ export default class CanvasDrawer {
     $bb.height = this._height;
     this._ctx = $bb.getContext("2d");
 
-    this._side = config.dimensions.items.width * config.ratio;
-    this._shift = (config.dimensions.items.width / 2) * config.ratio;
+    this._side = config.dimensions.items.width;
 
     this._customShiftX = 0;
     this._customShiftY = 0;
   }
 
   drawImage(image, posX, posY, posZ = 0, ratio = 1, shiftX = 0, shiftY = 0) {
-    console.log(this._size);
-    const x =
-      (posX - posY) * this._side -
-      config.dimensions.items.shiftX * config.ratio +
-      this._width / 2 -
-      this._customShiftX * this._side * config.ratio;
-    const y =
-      (posX + posY) * this._shift -
-      config.dimensions.items.shiftY * config.ratio +
-      this._shift +
-      this._customShiftY * this._side * config.ratio -
-      posZ * config.dimensions.items.shiftZ * config.ratio -
-      ((ratio - 1) * this._side) / 2;
-    this._ctx.drawImage(image, x, y);
+    // default calculus
+    let x = (posX - posY) * this._side;
+    let y = ((posX + posY) * this._side) / 2;
+    // remove blank
+    x -= config.dimensions.items.shiftX;
+    y -= config.dimensions.items.shiftY;
+    // we center the top left corner
+    x += this._width / (2 * config.ratio);
+    // we shift the element to match its correct place
+    x += this._side * this._customShiftX;
+    y += this._side * this._customShiftY;
+    // we manage the Z position
+    y -= posZ * this._side * 1.25;
+    // we shift the Y pos which allows to draw an avatar on the top left corner
+    y += this._side / 2;
+    this._ctx.drawImage(image, x * config.ratio, y * config.ratio);
   }
 
   clean() {
